f55d6e7c2ad41fe5f39420d203eaa987d6059ff7
TomDixon63 <35574025+TomDixon63@users.noreply.github.com>
2024-01-11 18:57:50 +0100

    Obsolete Sakai files deleted

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -15,7 +15,6 @@ import { AppLayoutComponent } from "./layout/app.layout.component";
                 ]
             },
             { path: 'auth', loadChildren: () => import('./manager/components/auth/auth.module').then(m => m.AuthModule) },
-            { path: 'landing', loadChildren: () => import('./manager/components/landing/landing.module').then(m => m.LandingModule) },
             { path: 'notfound', component: NotfoundComponent },
             { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
--- front/src/app/manager/components/dashboard/dashboard.component.ts
+++ front/src/app/manager/components/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { Product } from '../../api/product';
 import { Subscription } from 'rxjs';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
@@ -11,7 +10,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
     items!: MenuItem[];
 
-    products!: Product[];
+   
 
     chartData: any;
 
